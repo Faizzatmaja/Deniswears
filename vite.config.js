5a9639adbbac728ffe45124345d982c3c03b7b2b
faizzatmaja <faizatmaja@gmail.com>
2026-01-29 13:30:53 +0700

    hhhhh

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import laravel from 'laravel-vite-plugin';
 
 
 export default defineConfig({
-    base: '/',
+    base: '/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
